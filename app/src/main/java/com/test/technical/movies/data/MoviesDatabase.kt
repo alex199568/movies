93c3c400c2c665a7ab52ef842b3a28311ad8ed59
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2018-07-29 15:07:55 +0300

    Remove unused method

--- app/src/main/java/com/test/technical/movies/data/MoviesDatabase.kt
+++ app/src/main/java/com/test/technical/movies/data/MoviesDatabase.kt
@@ -1,33 +1,9 @@
 package com.test.technical.movies.data
 
 import android.arch.persistence.room.Database
-import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
-import android.content.Context
 
 @Database(entities = [Favourite::class], version = 1)
 abstract class MoviesDatabase : RoomDatabase() {
   abstract fun favouritesDao(): FavouritesDao
-
-  companion object {
-    private var instance: MoviesDatabase? = null
-
-    @JvmStatic
-    fun getInstance(context: Context): MoviesDatabase? {
-      if (instance == null) {
-        synchronized(MoviesDatabase::class) {
-          instance = Room.databaseBuilder(
-              context.applicationContext,
-              MoviesDatabase::class.java,
-              "movies.db"
-          ).build()
-        }
-      }
-      return instance
-    }
-
-    fun free() {
-      instance = null
-    }
-  }
 }
